fadaf8b9c587d1673a6265ec5095e05ca71bc174
Marcin Mider <marcin.mider@gmail.com>
2023-04-12 21:16:30 +0200

    removing warning from tests

--- test/slug_generator_test.dart
+++ test/slug_generator_test.dart
@@ -29,41 +29,41 @@ void main() {
     });
 
     test('Nested list 1', () {
-      List<String> _get(int i) => collections[0][i].toListOfStrings();
+      List<String> fetch(int i) => collections[0][i].toListOfStrings();
       expect(collections[0].numberOfTerms, 6);
-      expect(_get(0), ['a']);
-      expect(_get(1), ['b']);
-      expect(_get(2), ['c']);
-      expect(_get(3), ['d']);
-      expect(_get(4), ['e']);
-      expect(_get(5), ['f']);
+      expect(fetch(0), ['a']);
+      expect(fetch(1), ['b']);
+      expect(fetch(2), ['c']);
+      expect(fetch(3), ['d']);
+      expect(fetch(4), ['e']);
+      expect(fetch(5), ['f']);
     });
 
     test('Nested list 2', () {
-      List<String> _get(int i) => collections[1][i].toListOfStrings();
+      List<String> fetch(int i) => collections[1][i].toListOfStrings();
       expect(collections[1].numberOfTerms, 9);
-      expect(_get(0), ['g']);
-      expect(_get(1), ['h']);
-      expect(_get(2), ['i']);
-      expect(_get(3), ['e']);
-      expect(_get(4), ['f']);
-      expect(_get(5), ['a']);
-      expect(_get(6), ['b']);
-      expect(_get(7), ['c']);
-      expect(_get(8), ['d']);
+      expect(fetch(0), ['g']);
+      expect(fetch(1), ['h']);
+      expect(fetch(2), ['i']);
+      expect(fetch(3), ['e']);
+      expect(fetch(4), ['f']);
+      expect(fetch(5), ['a']);
+      expect(fetch(6), ['b']);
+      expect(fetch(7), ['c']);
+      expect(fetch(8), ['d']);
     });
 
     test('Cartesian list', () {
-      List<String> _get(int i) => collections[2][i].toListOfStrings();
+      List<String> fetch(int i) => collections[2][i].toListOfStrings();
       expect(collections[2].numberOfTerms, 8);
-      expect(_get(0), ['e', 'a']);
-      expect(_get(1), ['e', 'b']);
-      expect(_get(2), ['e', 'c']);
-      expect(_get(3), ['e', 'd']);
-      expect(_get(4), ['f', 'a']);
-      expect(_get(5), ['f', 'b']);
-      expect(_get(6), ['f', 'c']);
-      expect(_get(7), ['f', 'd']);
+      expect(fetch(0), ['e', 'a']);
+      expect(fetch(1), ['e', 'b']);
+      expect(fetch(2), ['e', 'c']);
+      expect(fetch(3), ['e', 'd']);
+      expect(fetch(4), ['f', 'a']);
+      expect(fetch(5), ['f', 'b']);
+      expect(fetch(6), ['f', 'c']);
+      expect(fetch(7), ['f', 'd']);
     });
   });
 }
